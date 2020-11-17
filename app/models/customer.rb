02a54da521e1d49c7067579a6b58217a5cadc3fc
harutokawabata <kawabata2317@gmail.com>
2020-11-17 14:58:12 +0000

    [add]mypage-header-footer-edit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,15 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
 end
+
+  # :last_name, :first_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted
