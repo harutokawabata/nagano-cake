41a95cfc3f7c440188fbabd2fd578c8913b7f6d3
harutokawabata <kawabata2317@gmail.com>
2020-11-18 13:59:54 +0000

    [add]something

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,29 @@
 Rails.application.routes.draw do
   # ここから（devise_forとルーティングがかぶるから別に記載)
-  get '/customers/:id/edit' => 'public/customers#edit'
+  # get '/customers/:id/edit' => 'public/customers#edit'
   patch '/customers/:id/update' => 'public/customers#update', as:'update_customer'
   # ここまで
-  devise_for :customers
-  devise_for :users
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
   scope module: :public do
-    resources :customers, only: [:show, :unsubscrib, :withdraw]
+    # get '/customers/:id/edit' => 'customers#edit', as: 'edit_customer'
+    resources :customers, only: [:edit, :show, :unsubscrib, :withdraw]
+    # :unsubscrib, :withdrawはresourcesでは働かないので20行目のように記載する必要がある
+    # resourcesではなくresourceを使うとURL直打ちでアクセスできないようになるがfind(params[:id])でピックアップできなくなる
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #homesコントローラーを利用してルートパス を設置
   get '/' => 'public/homes#top'
   get '/about' => 'public/homes#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_134908) do
+ActiveRecord::Schema.define(version: 2020_11_17_230613) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,16 +44,12 @@ ActiveRecord::Schema.define(version: 2020_11_12_134908) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
+
+# 先に顧客側を実装してしまったので後から管理者のログイン機能を実装しています
+
+# 「おおよその実行した順番」
+# rails g devise Admin
+# t.string :email
+# rails db:migrate
+# rails g devise:views admins
