566cded0fad2d366b426771caa15b17fd5067ca5
harutokawabata <kawabata2317@gmail.com>
2020-11-18 23:54:34 +0000

    [add]seed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,14 +4,14 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :email, presence: true
-  validates :postal_code, presence: true
-  validates :address, presence: true
-  validates :telephone_number, presence: true
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :email, presence: true
+  # validates :postal_code, presence: true
+  # validates :address, presence: true
+  # validates :telephone_number, presence: true
 end
 
   # :last_name, :first_name, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted
