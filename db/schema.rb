ae644f821c74450bfca871de5ffc391478c25017
harutokawabata <kawabata2317@gmail.com>
2020-12-01 23:15:59 +0000

    order-detail1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_134858) do
+ActiveRecord::Schema.define(version: 2020_12_01_144509) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 2020_11_27_134858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "genre_id"
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
