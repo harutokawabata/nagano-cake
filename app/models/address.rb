1fdab1dd69f48d55f3f5feb79aabdf31d7aaf825
harutokawabata <kawabata2317@gmail.com>
2020-12-01 14:33:30 +0000

    [add]add-order2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  belongs_to :customer
+    def personal_info
+     self.postal_code + self.name + self.address
+    # selfはここでいうとaddressになる（addressesのテーブルからもってくるから）
+     #selfは簡単にいうとpersonal_infoの前についてくる物
+    end
 end
--- db/migrate/20201125233635_create_orders.rb
+++ db/migrate/20201125233635_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.integer :customer_id
-      t.string :postal_coad
+      t.string :postal_code
       t.string :address
       t.string :name
       t.integer :shipping_cost
