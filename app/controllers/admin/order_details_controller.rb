e3db27f502459a03cfa614cd746438c0670014ac
harutokawabata <kawabata2317@gmail.com>
2020-12-13 09:01:50 +0000

    Finish

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -12,14 +12,23 @@ def show
 end
 
 def update
-  @order_detail = Order_detail.find(params[:id])
-  @order_detail.update
+  # @order_details = OrderDetail.all
+  @order_detail = OrderDetail.find(params[:id])
+  @order = @order_detail.order
+  @order_detail.update(order_detail_params)
+  if @order_detail.making_status == "making"
+     @order.update(status: "making")
+  end
+  if @order.order_details.count == OrderDetail.where(order_id: @order.id).where(making_status: "finish").count
+    #@order.order_detailsと同じ
+     @order.update(status: "perparation")
+  end
   redirect_back(fallback_location: admin_orders_path)
   # redirect_to admin_order_path
 end
 
 private
-  def order_params
+  def order_detail_params
   params.require(:order_detail).permit(:making_status)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_details
+  has_many :order_details, dependent: :destroy
   # enum payment_method: { card: 0, bank: 1}
   # enum payment_method: { 着手不可: 0, 入金待ち: 1, 入金確認:2, 発送準備中:3, 発送済み:4}
    enum status:{
     waiting:           0, #着手不可
     unstart:          1, #入金待ち
     confirm:   2, #入金確認
-    perparation:     3, #発送準備中
-    finish:     4, #発送済み
+    making:   3, #制作中
+    perparation:     4, #発送準備中
+    finish:     5, #発送済み
   }
    enum payment_method:{
     card: 0, #クレジットカード
